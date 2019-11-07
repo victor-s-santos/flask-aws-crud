079754bfd3877a45f2092d0d1b043c461dd20ff0
vss-py <victorsantos.py@gmail.com>
2019-11-07 13:34:49 -0300

    definindo o uso das variaveis

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import boto3
+from config import S3_BUCKET, S3_KEY, S3_SECRET
+
+s3 = boto.client(
+	's3',
+	aws_access_key_id=S3_KEY,
+	aws_secret_access_key=S3_SECRET)
 
 app = Flask(__name__)
 Bootstrap(app)
