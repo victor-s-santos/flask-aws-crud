fbd7433a906bb1ddde8bc437179614a11199fa45
vss-py <victorsantos.py@gmail.com>
2019-11-20 13:07:38 -0300

    flash message implementada

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 import boto3
 from config import S3_BUCKET, S3_KEY, S3_SECRET
@@ -11,6 +11,7 @@ s3_resource = boto3.resource(
 )
 app = Flask(__name__)
 Bootstrap(app)
+app.secret_key = 'secret'
 app.jinja_env.filters['datetimeformat'] = datetimeformat
 app.jinja_env.filters['file_type'] = file_type
 
@@ -33,6 +34,7 @@ def upload():
 	s3_resource = boto3.resource('s3')
 	my_bucket = s3_resource.Bucket(S3_BUCKET)
 	my_bucket.Object(file.filename).put(Body=file)
+	flash('Upload realizado com sucesso.')
 
 	return redirect(url_for('files'))
 	#return "Upload realizado com sucesso."
