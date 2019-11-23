d4dabf85e8096384d738ce6b9da2268e70ec5700
vss-py <victorsantos.py@gmail.com>
2019-11-22 21:18:07 -0300

    refatorando a suite de testes

--- tests.py
+++ tests.py
@@ -2,17 +2,23 @@ import unittest
 from app import *
 
 class TestHome(unittest.TestCase):
+
+	def setUp(self):
+		"""good development practices"""
+		app_test = app.test_client()
+		self.response = app_test.get('/')
+
 	def test_get(self):
 		"""Must return status code 200"""
-		app_test = app.test_client()
-		response = app_test.get('/')
-		self.assertEqual(200, response.status_code)
+		#app_test = app.test_client()
+		#response = app_test.get('/')
+		self.assertEqual(200, self.response.status_code)
 
 	def test_content_type(self):
 		"""Must return file type html""" 
-		app_test = app.test_client()
-		response = app_test.get('/')
-		self.assertIn('text/html', response.content_type)
-		
+		#app_test = app.test_client()
+		#response = app_test.get('/')
+		self.assertIn('text/html', self.response.content_type)
+
 if __name__ == '__main__':
 	unittest.main()
