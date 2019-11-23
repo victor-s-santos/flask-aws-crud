8a79faed419b19560c99dbd891d592a66f69af28
vss-py <victorsantos.py@gmail.com>
2019-11-22 21:11:54 -0300

    teste de verificar tipo de arquivo para template

--- tests.py
+++ tests.py
@@ -8,5 +8,11 @@ class TestHome(unittest.TestCase):
 		response = app_test.get('/')
 		self.assertEqual(200, response.status_code)
 
+	def test_content_type(self):
+		"""Must return file type html""" 
+		app_test = app.test_client()
+		response = app_test.get('/')
+		self.assertIn('text/html', response.content_type)
+		
 if __name__ == '__main__':
 	unittest.main()
